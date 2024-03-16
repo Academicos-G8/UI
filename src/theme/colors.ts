480b2d3cbba9a4f60104e5b867a183b20e0b824c
Felipe Mateus <felipe_mateus08@hotmail.com>
2024-03-16 16:55:13 -0300

    styles: colors and globals

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,23 +3,18 @@ export const colors = {
   transparent: 'transparent',
   current: 'currentColor',
 
-  // base colors
-  orange: '#FE962E',
-  blue: '#00C9FF',
-  red: '#F45E3F',
-  purple: '#5847EE',
-
   // semantic colors
-  base: '#0B0B0B',
-  primary: '#FFD600',
-  secondary: '#CAFF33',
-  high: '#F9F9F9',
-  low: '#A2A2A2',
-  elevation: {
-    1: '#161616',
-    2: '#212121',
-    3: '#424242',
+  primary: '#111111',
+  secondary: '#2a2a2a',
+  tertiary: '#0f0f0f',
+  high: '#eeeeee',
+  low: '#b4b4b4',
+  text: {
+    primary: '#eeeeee',
+    secondary: '#b4b4b4',
+    disabled: '#484848  ',
   },
+
   status: {
     info: '#00C9FF',
     danger: '#FE962E',
